cebd6c99e69856b558ebe934ed13feda98018f88
Aimane <aimaneyousr@gmail.com>
2023-09-13 10:14:33 -0700

    Write a function that prints the n times table, starting with 0. FIXED

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,10 +21,7 @@ _putchar(',');
 _putchar(' ');
 
 prod = num * multi;
-/*
- *put space if product is a single number
- *place the first digit if its two numbers
- */
+
 if (prod <= 9)
 _putchar(' ');
 if (prod <= 99)
@@ -32,7 +29,7 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + 48);
-_putchar((prod / 10) / 10 + 48);
+_putchar((prod / 10) % 10 + 48);
 } else if (prod <= 99 && prod >= 10)
 _putchar((prod / 10) + 48); /* get the first digit */
 _putchar((prod % 10) + 48); /*get the second digit */
