f580f9c8af0a368cc1eb770c3a6be75e7bde32b2
Aimane <aimaneyousr@gmail.com>
2023-09-15 10:44:24 -0700

    prints the numbers, from 0 to 9, followed by a new line. Fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -13,8 +13,8 @@ void print_numbers(void)
 	int n = 0;
 
 	do {
-		_putchar(num + 48);
+		_putchar(n + 48);
 		n++;
-	} while (num >= 0 && num <= 9);
+	} while (n >= 0 && n <= 9);
 	_putchar('\n');
 }
