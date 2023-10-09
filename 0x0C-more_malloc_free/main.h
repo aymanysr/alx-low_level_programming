3b1dbd37c28bf87cde216efacadf9f896e981edb
Aimane <aimaneyousr@gmail.com>
2023-10-09 14:22:51 -0700

    a function that concatenates two strings.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,8 @@ int _putchar(char c);
 void _puts(char *str);
 void *malloc_checked(unsigned int b);
 int *array_range(int min, int max);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+
 #endif /*MAIN_H*/
 
