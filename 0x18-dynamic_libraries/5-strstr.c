377d179a430bb881821b77911789dde31a1fc95b
aimane yousr <aimaneyousr@gmail.com>
2023-12-11 18:41:25 +0100

    a dynamic library libdynamic.so containing all the functions in README

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to be searched
+ * @needle: the substring to be located
+ *
+ * Return: If the substring is located - a pointer to the beginning
+ * If the substring is not located - NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+
+		haystack++;
+	}
+	return ('\0');
+}
